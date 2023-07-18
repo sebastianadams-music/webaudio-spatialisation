0fd68e9393c0b1c04969129a2370235816e6ab21
sebastianadams-music <72510504+sebastianadams-music@users.noreply.github.com>
2023-07-18 23:54:52 +0200

    began to improve file-switching incl adding chs

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+// for performance to stay the same when switching audio files, add a new layer of gain nodes which are "inputs" after the source, so that the source can be recreated and simply connect to those 8 source nodes.
+
 // banks of audio files
 const numberFiles = ["/sounds/1.wav", "/sounds/2.wav", "/sounds/3.wav", "/sounds/4.wav", "/sounds/5.wav", "/sounds/6.wav", "/sounds/7.wav", "/sounds/8.wav"]
-var vincent = ["/sounds/vi_snd_11_5aptRome_outsideChild.mp3", "/sounds/vi_snd_11_5Fire_sausage.mp3", "/sounds/vi_snd_11_5forge-230215-000.mp3", "/sounds/vi_snd_11_5In_Water.mp3", "/sounds/vi_snd_11_5metro-230219-000.mp3", "/sounds/vi_snd_11_5Tree_percussion.mp3", "/sounds/vi_snd_11_5walk_mainArea.mp3"]
+const vincent = ["/sounds/vi_snd_11_5aptRome_outsideChild.mp3", "/sounds/vi_snd_11_5Fire_sausage.mp3", "/sounds/vi_snd_11_5forge-230215-000.mp3", "/sounds/vi_snd_11_5In_Water.mp3", "/sounds/vi_snd_11_5metro-230219-000.mp3", "/sounds/vi_snd_11_5Tree_percussion.mp3", "/sounds/vi_snd_11_5walk_mainArea.mp3"]
 const outfoxing = ["/outfoxing.mp3", "/outfoxing.mp3", "/outfoxing.mp3", "/outfoxing.mp3", "/outfoxing.mp3", "/outfoxing.mp3", "/outfoxing.mp3", "/outfoxing.mp3"]
 const u2 = ["/sounds/u2-01.mp3", "/sounds/u2-02.mp3", "/sounds/u2-03.mp3", "/sounds/u2-04.mp3", "/sounds/u2-05.mp3", "/sounds/u2-06.mp3", "/sounds/u2-07.mp3", "/sounds/u2-08.mp3"]
-let audioFiles = u2
 
-let preLoadedFiles = []
-
-const preLoadFiles = async (files) => {preLoadedFiles = await filesToBuffers(files) }
 
 // set up click event for trigger script
 document.querySelector('#start').onclick = () => audioPlay(preLoadedFiles);
 document.querySelector('#auto').onclick = () => autoMoveListener();
-window.onload = x => preLoadFiles(u2)
+document.querySelector('#playnumbers').onclick = () => switchFiles(numberFiles);
+document.querySelector('#playu2').onclick = () => switchFiles(u2);
+
+document.querySelector('#loadfiles').onchange = () => loadFiles();
+document.querySelector('#playfiles').onchange = () => playFiles();
+window.onload = () => preLoadFiles(u2)
+
 // scale all distances 
 const distanceScale = 250
 const numberOfRadios = 32
+const movingSpeed = 12
 // create audio context
 const context = new AudioContext();
 this.context = context
@@ -31,29 +36,81 @@ this.listener = listener
 // declare and expose arrays of stuff
 let panners = []
 let sources = []
+let channels = []
 let receivers = []
 this.panners = panners
 this.sources = sources
 this.receivers = receivers
+this.channels = channels
+
+// triggers on page load (see above)
+const preLoadFiles = async (files) => {preLoadedFiles = await filesToBuffers(files) }
 
 // this trigger function called when the play button is pressed initially
-const audioPlay = async files => {
+const audioPlay = async bufs => {
   // let audioBuffers = await filesToBuffers(preLoadedFiles)
-  let audioBuffers = files
+  let audioBuffers = bufs
   console.log(audioBuffers)
 // create sources
-  for (let i = 0; i < 8; i++){
-    sources.push(createSource(audioBuffers[i], panners[i]))
-    sources[i].start(); // starts audio playback
+  for (let i = 0; i < audioBuffers.length; i++){
+    sources.push(createSource(audioBuffers[i]))
+    channels.push(context.createGain())
+    console.log(channels)
+    sources[i].connect(channels[i])
   }
+  
 // create receivers and panners
   for (let i = 0; i < numberOfRadios; i++){
     panners.push(newPannerNode()) 
-    receivers.push(newReceiver(sources, panners[i])) 
-    
-
+    console.log(channels)
+    receivers.push(newReceiver(channels, panners[i])) // outer list = panner; inner list = channel/radio
   }
-  };
+  sources.forEach((s) => s.start())
+
+}
+
+// to load and play a new set of audio files
+async function switchFiles(files){
+  newBuf = await filesToBuffers(files)
+  .then((newBuf) =>{for (let i = 0; i < sources.length; i++){
+    sources.forEach((s) => s.stop())
+  }
+  console.log(sources)
+  sources = []
+  console.log(sources)
+  for (let i = 0; i < newBuf.length; i++){
+    sources.push(createSource(newBuf[i]))
+    sources[i].connect(channels[i])
+  }
+  sources.forEach((s) => s.start())
+
+
+
+})
+  
+
+
+}
+
+function loadFiles() {
+  var e = document.getElementById("playfiles");
+  console.log(e.value)
+}
+
+function playFiles() {
+  var e = document.getElementById("playfiles");
+  console.log(e.value)
+  let ld = document.getElementById("loadfiles")
+  e
+}
+
+// to play a new audio file
+async function newAudioFile(files, i){
+  audioBuffers = await filesToBuffers(files)
+  sources[i].stop()
+  sources[i] = createSource(audioBuffers[i])
+}
+
 
 // functions
 
@@ -83,14 +140,16 @@ async function filesToBuffers(af) {
 //     // create a new sound source and panning node
 //   }
 
-function newReceiver(sources, panNode){
+function newReceiver(listOfSources, panNode){
   // receives sources
   // applies gains for each source
   // sends to one panner
   let g = []
-  for (let i = 0; i < sources.length; i++){
+  for (let i = 0; i < listOfSources.length; i++){
     let node = context.createGain()
-    sources[i%sources.length].connect(node)
+    node.gain.value = 0
+    console.log("test", i, listOfSources)
+    listOfSources[i%listOfSources.length].connect(node)
     g.push(node)
     node.connect(panNode)
   }
@@ -98,7 +157,7 @@ function newReceiver(sources, panNode){
   
 }
 
-function createSource(buffer, panner) {
+function createSource(buffer) {
   source = context.createBufferSource();
   source.buffer = buffer;
   // sources[i].playbackRate.value = randomInRange(.5, 2)
@@ -126,12 +185,6 @@ function newPannerNode(){
 
 
 
-// to play a new audio file
-async function newAudioFile(files, i){
-  audioBuffers = await filesToBuffers(files)
-  sources[i].stop()
-  sources[i] = createSource(audioBuffers[i], panners[i])
-}
 
 // functions to interface between p5 and WebAudio-----------------------
 
@@ -139,7 +192,7 @@ function radioPosition(circle, index){
   for (let i = 0; i < receivers.length; i++){
     // for every receiver, compute the gains of all 8 transmitters
 
-    for (let j = 0; j < receivers[i].length; j++){
+    for (let j = 0; j < receivers.length; j++){
       // get position of radio and check if it's in each circle
       
       // is radios[j] in the circle of each transmitter?
@@ -249,13 +302,13 @@ function autoMoveListener(){
 function handleKey(circle){
 
     if ((keyCode == RIGHT_ARROW) && (circle.x < (width - circle.d/2))){
-      circle.x++;
+      circle.x+= movingSpeed;
     } else if ((keyCode == LEFT_ARROW) && (circle.x > circle.d/2)){
-      circle.x--;
+      circle.x-= movingSpeed;
     } else if ((keyCode == UP_ARROW) && (circle.y > circle.d/2)) {
-      circle.y--;
+      circle.y-= movingSpeed;
     } else if ((keyCode == DOWN_ARROW) && (circle.y < (height - circle.d/2))){
-      circle.y++;
+      circle.y+= movingSpeed;
     }
     return circle
 }
